f3efee3bd16714d7a21929897dbb9b8e570d7529
chenyc <flyforest@outlook.com>
2021-01-21 22:27:58 +0800

    0183 0184

---
+++ mysql/0183.sql
@@ -0,0 +1,34 @@
+# 某网站包含两个表，Customers 表和 Orders 表。编写一个 SQL 查询，找出所有从不订购任何东西的客户。
+
+# Customers 表：
+
+# +----+-------+
+# | Id | Name  |
+# +----+-------+
+# | 1  | Joe   |
+# | 2  | Henry |
+# | 3  | Sam   |
+# | 4  | Max   |
+# +----+-------+
+# Orders 表：
+
+# +----+------------+
+# | Id | CustomerId |
+# +----+------------+
+# | 1  | 3          |
+# | 2  | 1          |
+# +----+------------+
+# 例如给定上述表格，你的查询应返回：
+
+# +-----------+
+# | Customers |
+# +-----------+
+# | Henry     |
+# | Max       |
+# +-----------+
+
+SELECT A.Name as Customers
+FROM Customers A
+LEFT JOIN Orders B
+ON A.id = B.CustomerId
+WHERE B.CustomerId IS NULL;
